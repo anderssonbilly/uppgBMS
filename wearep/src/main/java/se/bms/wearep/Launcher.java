4ae7dc393d2791dbeaed9011e4d9da3c0988fbe2
Billy Andersson <anderssonbilly@hotmail.com>
2018-09-30 13:58:09 +0200

    Created initial stage

--- wearep/src/main/java/se/bms/wearep/Launcher.java
+++ wearep/src/main/java/se/bms/wearep/Launcher.java
@@ -7,6 +7,11 @@ import javafx.stage.Stage;
 
 public class Launcher extends Application
 {
+	
+	private static final String TITLE = "Wearep - BMS";
+	private static final int WIDTH = 800;
+	private static final int HEIGHT = 800;
+	
     public static void main( String[] args )
     {
     	launch(args);
@@ -14,9 +19,14 @@ public class Launcher extends Application
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		Scene scene = new Scene(new Pane(),400,400);
+		
 		stage.setTitle("Wearep");
+		stage.setResizable(false);
+		
+		Scene scene = new Scene(new Pane(),WIDTH,HEIGHT);
 		stage.setScene(scene);
+		
 		stage.show();
 	}
+	
 }
