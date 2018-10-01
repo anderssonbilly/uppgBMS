183c289786078eda65fe542c5f65efb52abc8d62
Billy Andersson <anderssonbilly@hotmail.com>
2018-10-01 19:26:15 +0200

    Added some comments

--- wearep/src/main/java/se/bms/wearep/controller/TwitterController.java
+++ wearep/src/main/java/se/bms/wearep/controller/TwitterController.java
@@ -2,16 +2,19 @@ package se.bms.wearep.controller;
 
 import java.io.IOException;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 
 public class TwitterController extends AnchorPane {
 	@FXML
 	protected Pane twitter;
-
+	
 	@FXML
 	public void initialize() {
 		// if twitter logged in, change to tweet view
@@ -22,18 +25,30 @@ public class TwitterController extends AnchorPane {
 		// create new twitter object
 		// generate url
 		// change to url page
-		changeToBrowser();
+		changeToBrowser("http://www.google.se/"); // for tests
 	}
 
-	private void changeToBrowser() throws IOException {
+	@FXML
+	public void enterPin(ActionEvent event) {
+		String pin = ((TextField) ((Node)event.getSource()).getParent().getScene().lookup("#pin")).getText();
+		authenticate(pin);
+		// authenticate with the pin
+	}
+	
+	private void authenticate(String pin) {
+		// Check if successfull
+		// if true, go to tweets page
+		// if false go back to login
+	}
+	
+	private void changeToBrowser(String url) throws IOException {
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/webview.fxml"));
 		
 		twitter.getChildren().setAll((Parent)loader.load());
 		
 		WebViewController webView = loader.getController();
-		webView.changeWebpage("http://www.google.se/");
+		webView.changeWebpage(url); // should be the twitter login url
 		webView.setControls(new FXMLLoader(getClass().getResource("../view/logincon.fxml")));
 	}
-	
 	
 }
