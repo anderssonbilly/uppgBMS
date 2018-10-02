279032d9599ed8bd98e5404d73ccecc91aad55f8
Billy Andersson <anderssonbilly@hotmail.com>
2018-10-02 21:36:30 +0200

    Started to integrate twitter

--- wearep/src/main/java/se/bms/wearep/controller/TwitterController.java
+++ wearep/src/main/java/se/bms/wearep/controller/TwitterController.java
@@ -10,46 +10,49 @@ import javafx.scene.Parent;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import se.bms.wearep.twitter4j.Twitter4J;
 
 public class TwitterController extends AnchorPane {
 	@FXML
 	protected Pane twitter;
-	
+
+	private static Twitter4J twitter4j;
+
 	@FXML
 	public void initialize() {
-		// if twitter logged in, change to tweet view
+		if (twitter4j == null)
+			twitter4j = new Twitter4J();
 	}
-	
+
 	@FXML
 	public void twitterLogin() throws IOException {
-		// create new twitter object
-		// generate url
-		// change to url page
-		changeToBrowser("http://www.google.se/"); // for tests
+
+		changeToBrowser(twitter4j.getAuthURL()); // for tests
 	}
 
 	@FXML
 	public void enterPin(ActionEvent event) {
-		String pin = ((TextField) ((Node)event.getSource()).getParent().getScene().lookup("#pin")).getText();
+		String pin = ((TextField) ((Node) event.getSource()).getParent().getScene().lookup("#pin")).getText();
 		System.out.println("Pin: " + pin);
 		authenticate(pin);
-		// authenticate with the pin
 	}
-	
+
 	private void authenticate(String pin) {
-		// Check if successfull
-		// if true, go to tweets page
-		// if false go back to login
+		if (twitter4j.authorize(pin))
+			;
 	}
-	
+
 	private void changeToBrowser(String url) throws IOException {
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/webview.fxml"));
-		
-		twitter.getChildren().setAll((Parent)loader.load());
-		
+
+		twitter.getChildren().setAll((Parent) loader.load());
+
 		WebViewController webView = loader.getController();
 		webView.changeWebpage(url); // should be the twitter login url
 		webView.setControls(new FXMLLoader(getClass().getResource("../view/logincon.fxml")));
 	}
-	
+
+	public static Twitter4J getTwitter() {
+		return twitter4j;
+	}
 }
--- wearep/src/main/java/se/bms/wearep/controller/WeatherController.java
+++ wearep/src/main/java/se/bms/wearep/controller/WeatherController.java
@@ -11,15 +11,22 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
-import javafx.scene.text.Text;
 
 public class WeatherController {
 
 	@FXML
 	protected Pane weather;
-	
+
 	@FXML
 	protected TextField city;
+
+	@FXML
+	protected Button tweetWeatherBtn;
+	
+	@FXML
+	public void initialize() {
+		
+	}
 	
 	@FXML
 	public void getWeather(ActionEvent event) throws IOException {
@@ -29,35 +36,39 @@ public class WeatherController {
 		// show data in html file in the webview
 		changeToBrowser("http://www.smhi.se/");
 	}
-	
+
 	@FXML
 	public void tweetWeather(ActionEvent event) {
 		// tweet the current weather
-		System.out.println("Tweeting weather");
-		((Button)event.getSource()).disableProperty().set(true);
-		((Label)((Node)event.getSource()).getParent().getScene().lookup("#status")).setText("tweeting...");
+		if (TwitterController.getTwitter().isAuthorized()) {
+			System.out.println("Tweeting weather");
+			((Button) event.getSource()).disableProperty().set(true);
+			((Label) ((Node) event.getSource()).getParent().getScene().lookup("#status")).setText("tweeting...");
+		}else
+			System.err.println("Not authorized");
 	}
-	
+
 	private Double[] getCoords(String city) {
 		// get coords from city
 		return null;
 	}
-	
+
 	private void getWeatherData(Double[] coords) {
 		// get weather data from coords
 	}
-	
+
 	private void saveData() {
-		// save the data to disk 
+		// save the data to disk
 	}
-	
+
 	private void changeToBrowser(String url) throws IOException {
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("../view/webview.fxml"));
-		
-		weather.getChildren().setAll((Parent)loader.load());
-		
+
+		weather.getChildren().setAll((Parent) loader.load());
+
 		WebViewController webView = loader.getController();
 		webView.changeWebpage(url); // should be the twitter login url
 		webView.setControls(new FXMLLoader(getClass().getResource("../view/weathercon.fxml")));
 	}
+	
 }
