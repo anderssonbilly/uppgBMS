d10e1c40dc0bf64fa56f7d5d0bcc6dcaba027601
Koffo <koffov@gmail.com>
2018-10-05 21:51:29 +0200

    Moved main to it's proper location

---
+++ wearep/src/main/java/se/bms/wearep/Main.java
@@ -0,0 +1,13 @@
+package se.bms.wearep;
+import se.bms.wearep.coords.GetCoords;
+import se.bms.wearep.weather.*;
+public class Main {
+
+	public static void main(String[] args) {
+		GetCoords getcoords = new GetCoords();
+		getcoords.run("Piteå");
+		
+
+	}
+
+}
